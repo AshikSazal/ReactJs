2ed320a8eeab7cf85131de3af6956f3659398ece
Ashik Sazal <53079792+AshikSazal@users.noreply.github.com>
2022-09-21 12:26:01 +0600

    first commit

---
+++ 15-React-authentication/15.1-React-authentication-use-localstorage/src/store/auth-context.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+const AuthContext = React.createContext({
+  token: "",
+  isLoggedIn: false,
+  login: (token) => {},
+  logout: () => {},
+});
+
+export const AuthContextProvider = (props) => {
+  const initialToken = localStorage.getItem('token');
+  const [token, setToken] = useState(initialToken);
+
+  // This simply converts this truty or falsy value to a true or false boolean
+  // If token is not empty then return true, token is empty then return false
+  const userIsLoggedIn = !!token;
+
+  const loginHandler = (token) => {
+    setToken(token);
+    localStorage.setItem('token', token);
+  };
+
+  const logoutHandler = () => {
+    setToken(null);
+    localStorage.removeItem('token');
+  };
+
+  const contextValue = {
+    token: token,
+    isLoggedIn: userIsLoggedIn,
+    login: loginHandler,
+    logout: logoutHandler,
+  };
+
+  return (
+    <AuthContext.Provider value={contextValue}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+
+export default AuthContext;
---
+++ 15-React-authentication/README.md
@@ -0,0 +1,20 @@
+# Getting Started with Create React App
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## In this section I learned
+### 22 Adding Authentication To React Apps
+1. Module Introduction
+2. What, How & Why_
+4. Starting Setup & First Steps
+5. Adding User Signup5 Adding User Signup
+6. Showing Feedback To The User
+7. Adding User Login
+8. Managing The Auth State With Context
+9. Using The Token For Requests To Protected Resources
+10. Redirecting The User
+11. Adding Logout
+12. Protecting Frontend Pages
+13. Persisting The User Authentication Status
+14. Adding Auto-Logout
+15. Finishing Steps

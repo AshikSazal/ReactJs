e15d378cc79ddd06bf4e30a6306b2dc0a63220dd
Ashik Sazal <sojolislm@gmail.com>
2023-04-29 16:26:58 +0600

    first commit
    Single click span convert to input field

---
+++ 20. Extra/20.4. Single_click_convert_input/src/App.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import ElementMaker from "./ElementMaker";
+
+// Create an App component
+function App() {
+  // Set App's state
+  const [fullName, setFullName] = useState("Joe Abraham");
+  const [showInputEle, setShowInputEle] = useState(false);
+
+  const handleSave = (value) => {
+    setFullName(value);
+    console.log("Sending value to backend: ", value);
+  };
+
+  const handleDoubleClick = () => {
+    setShowInputEle(true);
+  };
+
+  const handleBlur = (value) => {
+    setShowInputEle(false);
+    console.log("Sending value to backend: ", fullName);
+  };
+
+  return (
+    <div>
+      <h1>Double-click the Full Name's Value to Edit</h1>
+      <div>
+        <strong>Full Name: </strong>
+        <ElementMaker
+          value={fullName}
+          handleSave={handleSave}
+          handleDoubleClick={handleDoubleClick}
+          handleBlur={handleBlur}
+          showInputEle={showInputEle}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ 20. Extra/20.4. Single_click_convert_input/src/ElementMaker.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+
+function ElementMaker(props) {
+  const [editedValue, setEditedValue] = useState(props.value);
+
+  const handleInputChange = (e) => {
+    setEditedValue(e.target.value);
+  };
+
+  const handleSave = () => {
+    props.handleSave(editedValue);
+    props.handleBlur();
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter' || e.type === 'blur') {
+      handleSave();
+    }
+  };
+
+  return (
+    <span>
+      {props.showInputEle ? (
+        <input
+          type="text"
+          value={editedValue}
+          onChange={handleInputChange}
+          onKeyDown={handleKeyDown}
+          onBlur={handleKeyDown}
+          autoFocus
+        />
+      ) : (
+        <span
+          onClick={props.handleDoubleClick}
+          style={{ display: 'inline-block', height: '25px', minWidth: '300px' }}
+        >
+          {props.value}
+        </span>
+      )}
+    </span>
+  );
+}
+
+export default ElementMaker;
